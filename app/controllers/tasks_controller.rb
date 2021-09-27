d0335a9e10a963dfbbdf3289c55995a7851a7810
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-27 13:31:52 +0900

    モデル設定

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,46 @@
 class TasksController < ApplicationController
+
   def index
+    @tasks = current_user.tasks
   end
 
   def show
+    @task = current_user.tasks.find(params[:id])
   end
 
   def new
+    @task = Task.new
+  end
+
+  def create
+    @task = current_user.tasks.new(task_params)
+    
+    if @task.save
+      redirect_to @task, notice: "タスク「#{@task.name}を登録しました。」"	
+    else
+      render :new
+    end
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
   end
+
+  def update
+    task = current_user.tasks.find(params[:id])
+    task.update!(task_params)
+    redirect_to tasks_url, notice: "タスク「#{task.name}を更新しました」"
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_url, notice: "タスクを「#{task.name}」削除しました。"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:name, :description)
+  end
+
 end
