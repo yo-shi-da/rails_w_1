df109179a08bfffe27d85c2b586a0fbc84648c3c
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-29 14:38:06 +0900

    カラム名追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -40,7 +40,7 @@ class TasksController < ApplicationController
 
   private
   def task_params
-    params.require(:task).permit(:name, :description)
+    params.require(:task).permit(:name, :description, :expired_at, :status, :priority)
   end
 
 end
