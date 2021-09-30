5200e4e8a2613bf68238c8340e777c52b10a54c5
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-09-30 20:06:33 +0900

    終了期限テスト完了

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,5 +2,7 @@ FactoryBot.define do
   factory :task do
     name { 'test_name' }
     description { 'test_description' }
+    expired_at { ' 002021-01-01 ' }
+    # expired_at { ' test_expired_at ' } #これでもテスクをクリアする。
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -4,41 +4,44 @@ RSpec.describe 'タスク管理機能', type: :system do
 
   describe '新規作成機能' do
     context 'タスクを新規作成した場合' do
-      it '作成したタスクが表示される' do
+      it 'リンクを押すと終了期限の降順に並び替えられる' do
         visit new_task_path
-        fill_in 'task_name', with: 'タスク名'
-        fill_in 'task_description', with: 'タスク詳細'
+        fill_in 'task_name', with: 'タスクです。'
+        fill_in 'task_description', with: 'タスク詳細です。'
+        fill_in 'task_expired_at', with: '002020-01-02'
         click_button '登録する'
-        expect(page).to have_content 'タスク詳細'
+        visit tasks_path
+        click_link 'sort_expired'
+        expect(all('tbody tr')[0]).to have_content '2020年01月02日'
       end
     end
   end
 
-  describe '一覧表示機能' do
-    context '一覧画面に遷移した場合' do
-      it '作成済みのタスク一覧が表示される' do
-        task = FactoryBot.create(:task, name: 'task')
-        visit tasks_path
-        expect(page).to have_content 'task'
-      end
-    end
+  # describe '一覧表示機能' do
+  #   context '一覧画面に遷移した場合' do
+  #     it '作成済みのタスク一覧が表示される' do
+  #       task = FactoryBot.create(:task, name: 'task')
+  #       visit tasks_path
+  #       expect(page).to have_content 'task'
+  #     end
+  #   end
     
-    context 'タスクが作成日時の降順に並んでいる場合' do
-      it '新しいタスクが一番上に表示される' do
-        task = FactoryBot.create(:task, name: '名称')
-        visit tasks_path
-        expect(all('tbody tr')[0]).to have_content '名称'
-      end
-    end
-  end
+  #   context 'タスクが作成日時の降順に並んでいる場合' do
+  #     it '新しいタスクが一番上に表示される' do
+  #       task = FactoryBot.create(:task, name: '名称')
+  #       visit tasks_path
+  #       expect(all('tbody tr')[0]).to have_content '名称'
+  #     end
+  #   end
+  # end
 
-  describe '詳細表示機能' do
-     context '任意のタスク詳細画面に遷移した場合' do
-       it '該当タスクの内容が表示される' do
-        task_a = FactoryBot.create(:task, name: '名称')
-        visit task_path(task_a)
-        expect(page).to have_content '名称'
-       end
-     end
-  end
+  # describe '詳細表示機能' do
+  #    context '任意のタスク詳細画面に遷移した場合' do
+  #      it '該当タスクの内容が表示される' do
+  #       task_a = FactoryBot.create(:task, name: '名称')
+  #       visit task_path(task_a)
+  #       expect(page).to have_content '名称'
+  #      end
+  #    end
+  # end
 end
