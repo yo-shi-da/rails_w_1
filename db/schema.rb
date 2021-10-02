473f3e0bd44cd4174fbe3a9bc50891bb7354ab83
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-10-02 19:45:47 +0900

    ユーザー管理

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_074837) do
+ActiveRecord::Schema.define(version: 2021_10_02_075833) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2021_10_02_074837) do
     t.string "password_digest", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
