8cb0d9c0f618bd587cee0486a3285407d8813b1e
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-10-03 14:07:18 +0900

    セッション機能テスト

--- spec/system/user_spec.rb
+++ spec/system/user_spec.rb
@@ -1,6 +1,71 @@
 require "rails_helper"
 
 describe 'ユーザー機能', type: :system do
+
+	# # OK
+	# describe 'セッション機能テスト' do
+	# 	context 'ログアウトしたとき' do			
+	# 		it 'ログアウトという言葉がある' do
+	# 			FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "password", password_confirmation: "password")
+	# 			visit login_path
+	# 			fill_in :session_email, with: 'a@example.com'
+	# 			fill_in :session_password, with: 'password'
+	# 			click_button 'ログインする'
+	# 			click_on 'ログアウト'
+	# 			page.driver.browser.switch_to.alert.accept
+	# 			expect(page).to have_content 'ログアウトしました。'
+	# 			# binding.pry
+	# 		end
+	# 	end
+	# end
+
+	# # OK
+	# describe 'セッション機能テスト' do
+	# 	context '一般ユーザが他人の詳細画面に飛ぶとき' do			
+	# 		it 'タスク一覧画面に遷移すること' do
+	# 			FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "password", password_confirmation: "password")
+	# 			visit login_path
+	# 			fill_in :session_email, with: 'a@example.com'
+	# 			fill_in :session_password, with: 'password'
+	# 			click_button 'ログインする'
+	# 			click_on '管理'
+	# 			page.driver.browser.switch_to.alert.accept
+	# 			expect(page).to have_selector 'h1', text: '「ユーザーA」のタスク一覧'
+	# 			# binding.pry
+	# 		end
+	# 	end
+	# end
+
+	# # OK
+	# describe 'セッション機能テスト' do
+	# 	context 'ログインしたとき' do			
+	# 		it '自分の詳細画面（マイページ）が表示される' do
+	# 			FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "password", password_confirmation: "password")
+	# 			visit login_path
+	# 			fill_in :session_email, with: 'a@example.com'
+	# 			fill_in :session_password, with: 'password'
+	# 			click_button 'ログインする'
+	# 			click_on 'Mypage'
+	# 			expect(page).to have_selector 'h1', text: '「ユーザーA」のMypage'
+	# 			# binding.pry
+	# 		end
+	# 	end
+	# end
+
+	# # OK
+	# describe 'セッション機能テスト' do
+	# 	context 'ログインしたとき' do			
+	# 		it '正常にログインできる' do
+	# 			FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "password", password_confirmation: "password")
+	# 			visit login_path
+	# 			fill_in :session_email, with: 'a@example.com'
+	# 			fill_in :session_password, with: 'password'
+	# 			click_button 'ログインする'
+	# 			expect(page).to have_selector 'h1', text: '「ユーザーA」のタスク一覧'
+	# 			# binding.pry
+	# 		end
+	# 	end
+	# end
 	
 	# # OK
 	# describe 'ユーザー登録' do
@@ -8,7 +73,6 @@ describe 'ユーザー機能', type: :system do
 	# 		it 'ログイン画面に遷移すること' do
 	# 			visit login_path
 	# 			click_on 'task一覧'
-	# 			# binding.pry
 	# 			expect(page).to have_selector 'h1', text: 'ログイン'
 	# 		end
 	# 	end
@@ -24,10 +88,8 @@ describe 'ユーザー機能', type: :system do
 	# 			fill_in :user_password, with: 'password'
 	# 			fill_in :user_password_confirmation, with: 'password'
 	# 			click_button '登録する'
-	# 			# binding.pry
 	# 			visit tasks_path
 	# 			expect(page).to have_selector 'h1', text: '「ユーザーA」のタスク一覧'
-	# 			# FactoryBot.create(:user, name: "ユーザーA", email: "a@example.com", password: "a@example.com, password_confirmation: "password")
 	# 		end
 	# 	end
 	# end
