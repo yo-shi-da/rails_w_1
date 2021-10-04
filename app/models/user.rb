8098143bce4bcf49481f363be82ecbc2650289d7
mitsutoshi yoshida <s.d.r.economy@gmail.com>
2021-10-04 17:00:31 +0900

    完了後修正2（update）

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,22 +12,25 @@ class User < ApplicationRecord
 
 	private
 
-	def admin_check_update
+	# このコードで良い。管理者しかユーザー変更はできない。よって、管理者以外（admin=false）は編集ができない。
+	# ということは、一般ユーザー（admin=false）は、ユーザー編集できず、編集可能は管理者（admin=true）ユーザーのみ。
+  def admin_check_update
 		@user = User.where(admin: true)
-		if @user.count == 1
-			self.admin = true
-		end
-	end
-	# if @user.count == 1 && self.admin == true
-	# throw :abort
-	# self.admin = false
-	# binding.pry
+    if @user.count == 1 && self.admin == false
+      throw :abort
+    end
+  end
 
 	def admin_check_destroy
 		@user = User.where(admin: true)
-		if @user.count == 1 && self.admin == true
+		if @user.count <= 1 && self.admin == true
 			throw :abort			
 		end
 	end
-
+	
 end
+
+# if @user.count == 1 && self.admin == true
+# throw :abort
+# self.admin = false
+# binding.pry
